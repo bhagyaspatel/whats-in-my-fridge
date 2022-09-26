98d7e1053a74371d2e38c8c4545eae406822f29a
bhagyaspatel <bhagyaspatel0911@gmail.com>
2022-09-26 18:19:04 +0530

    apikey updated

--- app/src/main/java/com/bhagyapatel/project/MVVM/ViewModal/MainViewModal.kt
+++ app/src/main/java/com/bhagyapatel/project/MVVM/ViewModal/MainViewModal.kt
@@ -5,16 +5,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bhagyapatel.project.DataClasses.RecipeItem
 import com.bhagyapatel.project.MVVM.Repository.RecipeRepository
+import com.bhagyapatel.project.Utils.Constants.APIKEY
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainViewModal(private val repository: RecipeRepository, val ingredients: String) : ViewModel() {
 
-    private val API_KEY = "1483da310768438380cdaf564ca241c6"
-
     init{
         viewModelScope.launch(Dispatchers.IO) {
-            repository.getRecipe(ingredients, API_KEY)
+            repository.getRecipe(ingredients, APIKEY)
         }
     }
 
